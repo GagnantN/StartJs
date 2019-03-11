ff2744c8b4985c91181b1c557a8779e8d3864a7b
Gagnant Nicolas <47555593+GagnantN@users.noreply.github.com>
2019-03-11 10:02:13 +0100

    Create hello-wilders.js

---
+++ hello-wilders.js
@@ -0,0 +1,10 @@
+// Exercie
+
+const nameMovie = 'Avatar';
+const date = '2009';
+const nameDirector = 'James Cameron';
+const message = 'My favorite movie is ' + nameMovie + 'for the date of ' + date + ' realize for ' + nameDirector;
+
+// popup ; 
+
+alert(message);
